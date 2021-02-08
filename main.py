ebdb957dcd539807647376b8aedb9d7319bc97c4
Seawed <seawed@bk.ru>
2021-02-08 14:18:58 +0300

    update
    create .CSV file and append write it.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 from datetime import datetime
+import pandas as pd
 
 if __name__ == '__main__':
     test = requests.get('https://www.cbr.ru/')
@@ -9,12 +10,14 @@ if __name__ == '__main__':
 
     with open("test.txt", "r", encoding="utf8") as f:
         contents = f.read()
-
         soup = BeautifulSoup(contents, 'lxml')
-
         curr = soup.findAll("div", {"class": "col-md-2 col-xs-9 _right mono-num"})
         dollar2 = curr[1].text.split()
         dollar2 = ''.join(dollar2)
         today = datetime.today()
         print('$ =', dollar2, 'time =', today.year,'-', today.month,'-', today.day)
-        #   print("Current day:", today.hour)
+        date = str(today.year)+'.' + str(today.month)+'.' + str(today.day)
+
+df = pd.DataFrame({'Date': [date],
+                   'Salary': [str(dollar2)[:-1]]})
+df.to_csv('my_csv.csv', mode='a', header=False)
